23caf912d4fcbaacbc19bb5ed2447674553f45ba
mat1506 <matutefrugone@gmail.com>
2024-10-21 23:27:08 -0300

    new

--- .binder/install.R
+++ .binder/install.R
@@ -1,5 +1,17 @@
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("tidyverse")
+# Establecer un repositorio de CRAN actualizado
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
+
+# Instalar paquetes con todas sus dependencias
+install.packages(c("ggplot2", "dplyr", "tidyverse", "MASS", "Matrix","remotes"), dependencies = TRUE)
+install.packages("XML")
 install.packages("sf")
+install.packages("units")
+install.packages("rnaturalearth")
+install.packages("here", dependencies = TRUE)
+install.packages("dplR")
+install.packages("rbacon")
+install.packages("neotoma2")
+remotes::install_github("nickmckay/lipdR",force = TRUE)
+remotes::install_github("nickmckay/geoChronR",force = TRUE)
+
 
